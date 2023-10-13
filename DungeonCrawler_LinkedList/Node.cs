73e261d021075eda208e72aa1ca51bacc70546bf
AaronSirico-Columbia <sirico@colum.edu>
2023-10-13 15:15:19 -0500

    Add project files.

---
+++ DungeonCrawler_LinkedList/Node.cs
@@ -0,0 +1,41 @@
+﻿//Credit: https://www.youtube.com/watch?v=GcC5kW9tyOQ&t=633s
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawlerLinkedLists
+{
+    public class Node
+    {
+        private int _data;
+        public int Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        private Node _next;
+        public Node Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+
+        private Node _previous;
+        public Node Previous
+        {
+            get { return _previous; }
+            set { _previous = value; }
+
+        }
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+
+    }
+}
